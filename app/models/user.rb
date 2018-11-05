0ada5a7521b5dbd2a6847b857e7fb0ce23a319ab
Kenta <ToM@Kenta-no-MacBook-Pro.local>
2018-11-05 19:20:09 +0900

    first commit

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  validates :name, presence: true
+  validates :name, length: { maximum: 15 }
+  validates :email, presence: true
+  validates :password, length: { minimum: 8, maximum: 32 }
+
+  has_secure_password
+  has_many :topics
+end
